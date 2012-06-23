6ce339a023e6f4f52e992ac09aa0af098a95bdf5
Brano <brano.kurajda@gmail.com>
2012-06-23 22:58:26 +0200

    pridal som vyhladavanie vo filmovej databaze podla herca, rezisera, nazvu filmu

--- EnterpriseApplication1/WebApp-war/src/java/managed/MoviesTable.java
+++ EnterpriseApplication1/WebApp-war/src/java/managed/MoviesTable.java
@@ -3,7 +3,9 @@ package managed;
 import beans.MovieFacade;
 import entity.Movie;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -23,7 +25,12 @@ public class MoviesTable {
     @EJB
     private MovieFacade movieFacade;
     int firstMovieDisplayed;
-    List<Movie> movies = new ArrayList<Movie>();
+    private List<Movie> movies = new ArrayList<Movie>();
+    
+    private String name = new String();
+    private String director = new String();
+    private String actor = new String();
+    
     
     /**
      * Constructor: Creates a new instance of BookTable 
@@ -45,6 +52,74 @@ public class MoviesTable {
     public String loadAllMovies() {
         movies = movieFacade.findAll();
         firstMovieDisplayed = 0;
+        return "movies";
+    }
+    
+    public void setDirector(String s){
+        director = s;
+    }
+    public void setName(String s){
+        name = s;
+    }
+    public void setActor(String s){
+        actor = s;
+    }
+    public String getDirector(){
+        return director;
+    }
+    public String getActor(){
+        return actor;
+    }
+    public String getName(){
+        return name;
+    }
+    public List<Movie> getMovies(){
+        return movies;
+    }
+    public String findMovies(){
+        
+        Set<Movie> byName = movieFacade.findByName(name);
+        Set<Movie> byDirector = new HashSet<Movie>();    //movieFacade.findByDirector(director);
+        Set<Movie> byActor = movieFacade.findByActor(actor);
+        
+        System.out.println("BBB " + byName.size() + " bActor " + byActor.size() );
+        
+        Set<Movie> nonEmpty = new HashSet<Movie>();
+        if( !byName.isEmpty() ){
+            nonEmpty = byName;
+        } else if (!byDirector.isEmpty()){
+            nonEmpty = byDirector;
+        } else if (!byActor.isEmpty()){
+            nonEmpty = byActor;
+        }
+        
+        if( nonEmpty.isEmpty() && 
+                (name.equals("") && actor.equals("") && director.equals(""))
+          )
+        {
+            loadAllMovies();
+            
+        } else {
+            if( name.equals("")){
+                byName = nonEmpty;
+            }
+            if( actor.equals("")){
+                byActor = nonEmpty;
+            }
+            if( director.equals("")){
+                byDirector = nonEmpty;
+            }
+
+           // System.out.println("all " + byName.size() + " " + byDirector.size() + "  " + byActor.size() );
+            // intersect sets
+            byDirector.retainAll(byActor);
+            byName.retainAll(byDirector);
+            
+         //   System.out.println("all " + byName.size() + " " + byDirector.size() + "  " + byActor.size() );
+            
+            movies = new ArrayList<Movie>(byName);
+            System.out.println("AAAAAAAAAAfound " + movies.size() + " movies");
+        }
         return "movies";
     }
 
