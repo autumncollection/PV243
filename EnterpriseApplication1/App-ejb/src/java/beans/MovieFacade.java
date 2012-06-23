228ff77293851d91096fbb9888516c38bcadc34a
Brano <brano.kurajda@gmail.com>
2012-06-23 22:58:26 +0200

    pridal som vyhladavanie vo filmovej databaze podla herca, rezisera, nazvu filmu

--- EnterpriseApplication1/App-ejb/src/java/beans/MovieFacade.java
+++ EnterpriseApplication1/App-ejb/src/java/beans/MovieFacade.java
@@ -8,6 +8,7 @@ import entity.Movie;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Collection;
 import java.util.Set;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -64,4 +65,57 @@ public class MovieFacade extends AbstractFacade<Movie> {
         
         return movies;        
     }
+    
+    public Set<Movie> findByActor(String actor){
+        // get movie ids with such actor
+        List<Long> ids = em.createNativeQuery("SELECT m.idMovie FROM Movie m, Person p, Actor a, ActorAtMovie am"
+                + "                          WHERE p.surname LIKE #surname AND "
+                + "                                p.idPerson = a.idPerson AND"
+                + "                                a.idActor = am.idActor AND"
+                + "                                am.idMovie = m.idMovie")
+                 .setParameter("surname", actor)
+                 .getResultList();
+        
+        Set<Movie> movies = new HashSet<Movie>();
+        
+        for( Long id: ids ){
+            Movie m = (Movie) em.createQuery("SELECT m FROM Movie m WHERE m.idMovie = ?1")
+                    .setParameter(1, id)
+                    .getSingleResult();
+            movies.add(m);
+        }
+        
+        return movies;
+               
+    }
+    
+    public Set<Movie> findByDirector(String director){
+        List<Long> ids = em.createNativeQuery("SELECT m.movieId FROM Movie m, Person p, Director d, DirectorAtMovie dm"
+                + "                          WHERE p.surname LIKE #surname AND "
+                + "                                p.idPerson = d.idPerson AND"
+                + "                                d.idDirector = dm.idDirector AND"
+                + "                                dm.idMovie = m.idMovie")
+                 .setParameter("surname", director)
+                 .getResultList();
+        
+        Set<Movie> movies = new HashSet<Movie>();
+        
+        for( Long id: ids ){
+            Movie m = (Movie) em.createQuery("SELECT m FROM Movie m WHERE m.idMovie = ?1")
+                    .setParameter(1, id)
+                    .getSingleResult();
+            movies.add(m);
+        }
+        
+        return movies;
+    }
+    
+    public Set<Movie> findByName(String movieName){
+        List<Movie> movies = em.createNamedQuery("Movie.findByMovieName")
+                 .setParameter("movieName", movieName)
+                 .getResultList();
+        
+        return new HashSet<Movie>(movies);
+    }
+    
 }
